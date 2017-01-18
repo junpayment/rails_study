49a903eb314579683ba9509633b9ee79d3905c78
nomura <nomura-junpei@dmm.com>
2017-01-18 18:17:04 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページ毎の完全なタイトルを返す。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title # 暗黙の戻り値
+    else
+      page_title + " | " + base_title # 暗黙の戻り値
+    end
+  end
 end
