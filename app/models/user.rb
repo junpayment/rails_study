e0cfd9b3fda0b6750766092e73c870d182d4952a
nomura <nomura-junpei@dmm.com>
2017-02-01 18:49:05 +0900

    https://railstutorial.jp/chapters/account_activation?version=5.0#sec-account_activation_emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50}
 
@@ -40,5 +41,15 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+private
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
